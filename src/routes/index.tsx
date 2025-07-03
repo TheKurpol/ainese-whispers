35156d6d36a34c92247d0b17df2de23c60e8beda
TheKurpol <kkurzydlo0@gmail.com>
2025-07-03 23:03:19 +0200

    main menu prototype idk

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import logo from '../logo.svg'
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -7,33 +10,28 @@ export const Route = createFileRoute('/')({
 
 function App() {
   return (
-    <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='justify-center items-center'>
+        <h1 className='text-4xl text-center font-bold mb-8'>AInese Whispers</h1>
+      </div>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+        <Card className='p-6 w-3xl shadow-lg flex justify-center items-center'>
+          <Input
+            placeholder='Nickname'
+            className='w-[400px] h-16'
+          />
+          <div className='flex gap-3'>
+            <Input
+              placeholder='Party ID'
+              className='w-[195px] h-16 text-8xl'
+            />
+            <Button className='h-16 w-[195px] text-xl'>
+              Join Party
+            </Button>
+          </div>
+          <Button className='w-[400px] h-16 text-xl'>Create New Party</Button>
+        </Card>
+      </div>
+    </>
   )
 }
